b5c8ce3f220c5eb766d1bc5241cbe31362e5d414
Tomasz Odrzywolek <tomeko92@gmail.com>
2018-11-25 14:07:18 +0100

    add simple echo client

---
+++ EchoClient/src/pl/todrzywolek/client/EchoClient.java
@@ -0,0 +1,38 @@
+package pl.todrzywolek.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoClient {
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 8000)) {
+            BufferedReader echoes = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
+
+            Scanner scanner = new Scanner(System.in);
+            String echoString;
+            String response;
+
+            do {
+                System.out.println("Enter string to be echoed: ");
+                echoString = scanner.nextLine();
+
+                stringToEcho.println(echoString);
+                if (!echoString.equals("exit")) {
+                    response = echoes.readLine();
+                    System.out.println(response);
+                }
+            } while (!echoString.equals("exit"));
+
+
+        } catch (IOException e) {
+            System.out.println("Client error: " + e.getMessage());
+        }
+    }
+}

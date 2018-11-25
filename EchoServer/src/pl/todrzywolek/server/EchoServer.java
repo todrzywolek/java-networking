bdf3b5bd74f8315b5ba01f50e3163604fbf89002
Tomasz Odrzywolek <tomeko92@gmail.com>
2018-11-25 15:24:43 +0100

    add handling connections in separate threads

--- EchoServer/src/pl/todrzywolek/server/EchoServer.java
+++ EchoServer/src/pl/todrzywolek/server/EchoServer.java
@@ -1,28 +1,14 @@
 package pl.todrzywolek.server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class EchoServer {
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(8000)) {
-            Socket socket = serverSocket.accept();
-            System.out.println("Client connected");
-            BufferedReader input = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
             while (true) {
-                String echoString = input.readLine();
-                if (echoString.equals("exit")) {
-                    break;
-                }
-                output.println("Echo from server: " + echoString);
+                new Echoer(serverSocket.accept()).start();
             }
 
         } catch (IOException e) {
